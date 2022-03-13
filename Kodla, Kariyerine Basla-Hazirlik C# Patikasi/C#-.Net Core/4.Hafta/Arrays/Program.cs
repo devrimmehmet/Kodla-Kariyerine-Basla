461f509f0fa208e7c0fb2320ee77d96b11a7043f
devrimmehmet <devrimmehmet@gmail.com>
2022-03-13 23:25:11 +0300

    arraylerle ilgili notlar eklendi.

---
+++ Kodla, Kariyerine Basla-Hazirlik C# Patikasi/C#-.Net Core/4.Hafta/Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sayiDizisi= {23,12,4,86,72,3,11,17};
+            Console.WriteLine("***** Sırasız Liste *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            
+            Console.WriteLine("***** Sıralı Liste *****");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("***** Array Clear *****");
+            Array.Clear(sayiDizisi,2,2);//sayı dizisi elemanlarını kullanarak 2.indexten başlayarak 2tane indexi 0ladı.
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("***** Reverse *****");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("***** IndexOf *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+            
+            Console.WriteLine("***** ReSize *****");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
